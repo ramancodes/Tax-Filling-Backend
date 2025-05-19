38309a4f7b75b3543ab5e23e7fefb0c29d7423f6
ramancodes <ramanmanjhi01@gmail.com>
2025-05-19 06:50:17 +0530

    feat: added admin side features

--- src/controllers/adminControllers/alltaxApplications.js
+++ src/controllers/adminControllers/alltaxApplications.js
@@ -3,7 +3,16 @@ const { Models } = require('../../models/index');
 module.exports = {
     getAllTaxes: async (req, res)=>{
         try {
-            const taxes = await Models.incomeTaxModel.findAndCountAll({});
+            const taxes = await Models.incomeTaxModel.findAndCountAll({
+                include: [
+                    {
+                    model: Models.user
+                    }
+                ]
+            });
+            if (!taxes) {
+                return res.status(404).json({ message: 'No taxes found' });
+            }
 
             res.status(200).json({
               message: "Taxes fetched successfully!",
